78abdf3ce56cd5e47ae0461a9f6e5adbcb2d6476
Jakub Radawiec <jak@wp.pl>
2020-04-12 20:52:25 +0200

    news filter front

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-   db:'',
+   db:`mongodb+srv://wilk17:9Rql4437uFahAk2z@cluster0-dy6ah.mongodb.net/test?retryWrites=true&w=majority`,
    keySession:['TwojKLUCZ'],
    maxAgeSession: 24*60*60*1000,
 }
