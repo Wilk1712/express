6e1e5a61f75b09559fc0af668de72f30cf55f06e
Jakub Radawiec <jak@wp.pl>
2020-04-13 08:50:23 +0200

    err

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
+var apiRouter = require('./routes/api');
 
 
 
@@ -51,6 +52,7 @@ app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 app.use('/news', newsRouter);
 app.use('/quiz', quizRouter);
+app.use('/api', apiRouter);
 
 
 // catch 404 and forward to error handler
